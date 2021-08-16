ee7ea190439100de26f6717cdcc5a30580e6b74e
quyln065 <lnquy065@gmail.com>
2021-08-16 12:07:52 +0700

    Updates

---
+++ precache-manifest.df8f99c8bf86dd915051fd205f74da8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15229b0e2542449c820a29a2d310698d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf8fee24e7f23349d359",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "dad0427140e0ea7ed3ff",
+    "url": "./static/js/2.a508a01c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a508a01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8fee24e7f23349d359",
+    "url": "./static/js/main.3950fed6.chunk.js"
+  },
+  {
+    "revision": "73f7c6e663cd1f9f97f3",
+    "url": "./static/js/runtime-main.5c764ae2.js"
+  }
+]);
